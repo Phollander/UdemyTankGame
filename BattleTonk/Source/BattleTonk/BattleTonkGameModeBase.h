50912fb2b59d71b7d450af510d48ed97b5fa5d3c
Patrick@existential.nl <patrick@existential.nl>
2019-11-08 14:38:58 +0100

    added battletank base project

---
+++ BattleTonk/Source/BattleTonk/BattleTonkGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTonkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETONK_API ABattleTonkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
